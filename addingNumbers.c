2eb4e3a5323677a40b297b89f2247d226abd43a7
akijikarandolphyufenyuy <akijikarandolphyufenyuy@gmail.com>
2025-12-15 15:32:22 +0100

    now lets get the avarage of these two numbers

--- addingNumbers.c
+++ addingNumbers.c
@@ -7,6 +7,10 @@ int main()
     printf("Enter the second number: ");
     scanf("%d",&num2);
     sum=num1+num2;
-printf("The sum of %d and %d is %d\n",num1,num2,sum);
+    printf("The sum of %d and %d is %d\n",num1,num2,sum);
+//now lets get the avarage of these two numbers
+float average;
+average=sum/2.0;
+printf("The average of %d and %d is %.2f\n",num1,num2,average);
     return 0;
 }
