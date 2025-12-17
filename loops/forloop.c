89fee8aba69378563a96867ca12b994a0ff17a07
akijikarandolphyufenyuy <akijikarandolphyufenyuy@gmail.com>
2025-12-17 09:56:40 +0100

    printing numbers from 1 to 10

---
+++ loops/forloop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    // this program will print the first 10 numbers using for loops from 1 to 1o
+    //we use the for loop when we know the number of times we have to iterate 
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
